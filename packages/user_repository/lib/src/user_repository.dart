0d397ae5e0408557543adf1c41861b13eff14e0b
BINIYAM <biniabera274@gmail.com>
2022-02-28 12:52:56 +0300

    edit profile page done !

--- packages/user_repository/lib/src/user_repository.dart
+++ packages/user_repository/lib/src/user_repository.dart
@@ -4,15 +4,18 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:user_repository/utility/util.dart';
 import 'models/models.dart';
+class UserNotFoundException implements Exception {}
 
 class UserRepository {
   User? _user;
-
+User? get userGetter {
+    return _user;
+  }
  Future<bool> uploadUserImage(dynamic images) async {
+   print("Uploading Image!");
    return true;
  }
   Future<User?> getUser() async {
-    if (_user != null) return _user;
     var token = await Util.getToken("token");
     token = token!;
     final headers = {
@@ -27,11 +30,83 @@ class UserRepository {
       headers: headers,
     ).timeout(const Duration(seconds: 15));
     if (response.statusCode == 200) {
-      final user = User.fromDatabaseJson(json.decode(response.body));
-      
-      return user;
+      return User.fromDatabaseJson(json.decode(response.body));
     } else {
-      throw Exception(json.decode(response.body));
+       throw UserNotFoundException();
     }
+  }
+  Future<bool> editProfilePicture(dynamic profile) async {
+    var token = await Util.getToken("token");
+    token = token!;
+    final headers = {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+    await dotenv.load();
+    final API_FILE_URL = dotenv.get("API_FILE_URL", fallback: "API_FILE_URL not found");
+    var response = await http.put(
+      Uri.parse(API_FILE_URL),
+      headers: headers,
+      body:  {
+        "file": profile
+      }
+    ).timeout(const Duration(seconds: 15));
+    if (response.statusCode == 200) {
+      print(json.decode(response.body)[0]);
+      final pro =  Profile.fromDatabaseJson(json.decode(response.body)[0]);
+      print(pro.toString());
+      return pro;
+    } else {
+       throw UserNotFoundException();
+    }
+  }
+
+  Future<Profile> editProfile(Profile profile) async {
+    var token = await Util.getToken("token");
+    token = token!;
+    final headers = {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+    await dotenv.load();
+    final BASE_URL = dotenv.get("BASE_URL", fallback: "BASE_URL not found");
+    final PROFILE_URL = dotenv.get("API_USER_URL", fallback: "API_USER_URL not found");
+    var response = await http.put(
+      Uri.parse(PROFILE_URL),
+      headers: headers,
+      body: profile.toDatabaseJson()
+    ).timeout(const Duration(seconds: 15));
+    if (response.statusCode == 200) {
+      print(json.decode(response.body)[0]);
+      final pro =  Profile.fromDatabaseJson(json.decode(response.body)[0]);
+      print(pro.toString());
+      return pro;
+    } else {
+       throw UserNotFoundException();
+    }
+
+  }
+  Future<Profile?> getProfile() async {
+   var token = await Util.getToken("token");
+    token = token!;
+    final headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+    await dotenv.load();
+    final BASE_URL = dotenv.get("BASE_URL", fallback: "BASE_URL not found");
+    final PROFILE_URL = dotenv.get("PROFILE_URL", fallback: "PROFILE_URL not found");
+    var response = await http.get(
+      Uri.parse(PROFILE_URL),
+      headers: headers,
+    ).timeout(const Duration(seconds: 15));
+    if (response.statusCode == 200) {
+      final pro =  Profile.fromDatabaseJson(json.decode(response.body));
+      print(pro.toString());
+      return pro;
+    } else {
+       throw UserNotFoundException();
+    } 
   }
 }
